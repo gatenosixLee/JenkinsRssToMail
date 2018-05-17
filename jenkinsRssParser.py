729d669dbd2053c45f9fde622abfc97e064bf499
gatenosix lee <gatenosix@gmail.com>
2018-05-18 00:20:34 +0900

    첫 커밋

--- jenkinsRssParser.py
+++ jenkinsRssParser.py
@@ -1,3 +1,33 @@
+from jinja2 import Template
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-print('test')
+me = 'gatenosix@gmail.com'
+you = 'gatenosix@gmail.com'
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = 'test'
+msg['From'] = me
+msg['To'] = you
+
+text = 'test message'
+
+f = open('mailTemplate.html', 'r')
+template = Template(f.read())
+f.close()
+
+html = template.render()
+
+part2 = MIMEText(html, 'html')
+
+msg.attach(part2)
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.login(me, 'dlckdqo08907!@#$')
+
+server.send_message(msg)
+server.quit()
+
